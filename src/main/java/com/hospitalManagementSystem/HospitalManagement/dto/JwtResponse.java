e676468cbf2fd404474f759de60115650efbc737
Abhiram <abhirampandey54@gmail.com>
2025-05-20 11:27:05 +0530

    jwt updated

--- src/main/java/com/hospitalManagementSystem/HospitalManagement/dto/JwtResponse.java
+++ src/main/java/com/hospitalManagementSystem/HospitalManagement/dto/JwtResponse.java
@@ -3,9 +3,11 @@ package com.hospitalManagementSystem.HospitalManagement.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class AuthResponse {
 
     private String token;
